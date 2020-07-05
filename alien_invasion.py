c4d0920ae73372bf71d5f05f47806f895f3df0bc
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-07-05 16:56:41 -0700

    adds movement left

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,11 +34,16 @@ class AlienInvasion:
         sys.exit()
       elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT:
-          #move the ship to the right.
           self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+          self.ship.moving_left = True
+
       elif event.type == pygame.KEYUP:
         if event.key == pygame.K_RIGHT:
           self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+          self.ship.moving_left = False
+        
   
   def _update_screen(self):
     # redraw the screen during each pass through the loop.
