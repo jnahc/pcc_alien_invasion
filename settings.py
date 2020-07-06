3340bbf1fdc0904ab18c8fe5e2123a5d84cfa5d5
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-07-05 17:01:49 -0700

    adds setting for ship speed - currently defaults to 1.5x instead of 1x

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings:
 
   def __init__(self):
     """initialize the game's settings."""
-    #screens ettings
+    #screen settings
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (230, 230, 230)
+
+    #ship settings
+    self.ship_speed = 1.5
