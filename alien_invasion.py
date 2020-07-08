f5cb5579a7486e5ab10c8c6051f3e2ea8a8b5e1c
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-07-08 14:26:19 -0700

    creates a row of aliens, creates method for creating aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,9 +79,25 @@ class AlienInvasion:
 
   def _create_fleet(self):
     """create the fleet of aliens"""
-    #make an alien
+    #create an alien and find the number of aliens in a row
+    #spacing between each alien is equal to one alien widdth
     alien = Alien(self)
-    self.aliens.add(alien)
+    alien_width = alien.rect.width
+    available_space_x = self.settings.screen_width - (2 * alien_width)
+    number_aliens_x = available_space_x // (2 * alien_width)
+
+    #create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+      self._create_alien(alien_number)
+
+  
+  def _create_alien(self, alien_number):
+    """create an alien and place it in the row"""
+      alien = Alien(self)
+      alien_width = alien.rect.width
+      alien.x = alien_width + 2 * alien_width * alien_number
+      alien.rect.x = alien.x
+      self.aliens.add(alien)
 
   def _update_screen(self):
     # redraw the screen during each pass through the loop.
