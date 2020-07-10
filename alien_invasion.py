9c2a62927ea7dfa72a8d003916296f2dfe38c365
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-07-09 17:06:24 -0700

    increases difficulty when aliens are cleared

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,6 +65,8 @@ class AlienInvasion:
     """start a new game when the player clicks play"""
     button_clicked = self.play_button.rect.collidepoint(mouse_pos)
     if button_clicked and not self.stats.game_active:
+      #reset the game settings
+      self.settings.initialize_dynamic_settings()
       #reset the game statistics
       self.stats.reset_stats()
       self.stats.game_active = True
@@ -127,6 +129,7 @@ class AlienInvasion:
       #destroy existing bullets and create new fleet
       self.bullets.empty()
       self._create_fleet()
+      self.settings.increase_speed()
 
   def _create_fleet(self):
     """create the fleet of aliens"""
