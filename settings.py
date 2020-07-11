a17c1521e04d68324f063924bc7cf8135dbbfe7e
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-07-10 21:25:55 -0700

    added score rounding, decimal places

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
     self.ship_limit = 3
 
     #bullet settings
-    self.bullet_width = 3
+    self.bullet_width = 300 #3
     self.bullet_height = 15
     self.bullet_color = 60, 60, 60
     self.bullets_allowed = 3
@@ -30,7 +30,7 @@ class Settings:
 
   def initialize_dynamic_settings(self):
     """initialize settings that change throughout the game."""
-    self.ship_speed = 1.5
+    self.ship_speed = 20 #1.5
     self.bullet_speed = 3.0
     self.alien_speed = 1.0
 
@@ -47,4 +47,3 @@ class Settings:
     self.alien_speed *= self.speedup_scale
 
     self.alien_points = int(self.alien_points * self.score_scale)
-    print(self.alien_points)
